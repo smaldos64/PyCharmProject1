6953cc194e9caeb66b98e3a11b654911f48e25ed
Lars Thise Pedersen <ltpe@techcollege.dk>
2018-08-27 11:15:56 +0200

    Initial commit af PyCharm Project.

---
+++ Main.py
@@ -0,0 +1,30 @@
+class Module_Test:
+
+    def Add_Numbers(Number1, Number2):
+        Number3 = Number1 + Number2
+        return Number3
+
+if __name__ == '__main__':
+    print("Hej med dig !!!")
+
+    var1 = 10
+
+    if (10 == var1):
+        print("Værdien af var1 er 10")
+        print("Og nu kører det for os")
+    else:
+        print("Værdien af var1 er forskellig fra 10")
+        print("Og nu kører det ikke for os")
+
+    print("Her kører jeg altid !!!")
+
+    var2 = 12
+    var3 = 24
+
+    var4 = Module_Test.Add_Numbers(var2, var3)
+
+    print("Resultatet af %d + %d er %d" % (var2, var3, var4))
+
+    var4 = Module_Test.Add_Numbers(23, 78)
+
+    print("Resultatet af %d + %d er %d" % (23, 78, var4))
---
+++ test_add_Numbers.py
@@ -0,0 +1,22 @@
+import unittest
+#from unittest import TestCase
+
+import Main
+from Main import Module_Test
+
+class TestAdd_Numbers(unittest.TestCase):
+    def test_Add_Numbers(self):
+        var3 = Main.Module_Test.Add_Numbers(10, 12)
+        self.assertEqual(22, var3)
+        var3 = Module_Test.Add_Numbers(20, 32)
+        self.assertEqual(52, var3)
+        #self.fail()
+
+    def test_Add_Numbers1(self):
+        var3 = Main.Module_Test.Add_Numbers(32, 44)
+        self.assertEqual(76, var3)
+        var3 = Module_Test.Add_Numbers(78, 22)
+        self.assertEqual(100, var3)
+
+if __name__ == '__main__':
+    unittest.main()
